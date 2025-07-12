6693999583a1a473c6ef6f6730bd78b1973170ac
dyd1833842773 <142762424+dyd1833842773@users.noreply.github.com>
2025-07-12 16:41:58 +0800

    Update xiangguanxishu.py

--- xiangguanxishu.py
+++ xiangguanxishu.py
@@ -2,7 +2,7 @@ import pandas as pd
 from scipy.stats import pearsonr, spearmanr
 
 # 读取 Excel 文件
-data = pd.read_excel('./15k237-data/eccel/complex/sampling_results_60_1.xlsx')
+data = pd.read_excel('./sampling_results_60_1.xlsx')
 
 # 提取第七列特征和倒数第二列标签
 feature_seventh = data.columns[2]  # 第七列特征
@@ -15,7 +15,7 @@ pearson_corr, pearson_p_value = pearsonr(data[feature_seventh], data[label])
 spearman_corr, spearman_p_value = spearmanr(data[feature_seventh], data[label])
 
 # 输出结果
-print("第七列特征和倒数第二列标签的皮尔森相关系数:", pearson_corr)
+print("皮尔森相关系数:", pearson_corr)
 print("皮尔森相关系数的 p 值:", pearson_p_value)
-print("第七列特征和倒数第二列标签的斯皮尔曼相关系数:", spearman_corr)
+print("斯皮尔曼相关系数:", spearman_corr)
 print("斯皮尔曼相关系数的 p 值:", spearman_p_value) 
